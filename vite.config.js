74672dcf850f4ec19ad5f47c598d73d4de135bb4
CIquIkyu <princessoflovevenus@gmail.com>
2023-06-19 18:18:13 +0630

    try

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ export default {
     rollupOptions: {
       input: {
         main: "./index.html",
-        //src: "./src/whatshypmic.html",
-       // src: "./src/character.html",
-        //src: "./src/story.html",
-        src: "./src/sound.html",
         whatshypmic: "./whatshypmic/whatshypmic.html",
+        story: "./story/story.html"
       },
        
   },
