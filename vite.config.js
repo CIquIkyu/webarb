689c1f6d92823aafde20667aef60a8ecbc05d8d4
CIquIkyu <princessoflovevenus@gmail.com>
2023-06-19 20:46:40 +0630

    last?

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ export default {
         story: "./story/story.html",
         character: "./character/character.html",
         sound: "./sound/sound.html",
-        S1Page: "./S1Page/S1Page.html"
+        S1Page: "./S1Page/S1Page.html",
+        S2Page: "./S2Page/S2Page.html",
+        Cha: "./Cha/cha.html"
       },
   },
 }
