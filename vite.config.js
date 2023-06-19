413d43a7f3dbbf2bba13dd7976226c2b98d943ac
CIquIkyu <princessoflovevenus@gmail.com>
2023-06-19 18:51:09 +0630

    test

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,13 @@ export default {
         story: "./story/story.html",
         character: "./character/character.html",
         sound: "./sound/character.html",
+      },
+  },
+    rollupOptions: {
+      input: {
+        story: "./story/story.html",
         S1Page: "./S1Page/S1Page.html"
       },
-       
-  },
+}
 }
 }
