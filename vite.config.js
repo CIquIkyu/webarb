35eeec9fad1013742662b1d16347873f42dc5a95
CIquIkyu <princessoflovevenus@gmail.com>
2023-06-19 15:27:10 +0630

    inpuy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ export default {
       input: {
         main: "./index.html",
         products: './src/whatshypmic.html',
-        products: './src/character.html',
-        products: './src/story.html',
-        products:'./src/sound.html'
       },
     },
   },
